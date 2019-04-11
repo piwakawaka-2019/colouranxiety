ea0ba81516262930f2d6f68bd46ba17cbf400edc
SharleneRyan <sa.ryan@live.com>
2019-04-12 11:48:27 +1200

    Save for nanz incert div

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,13 +7,13 @@ class Header extends Component {
     }
     render() { 
         return (
-<<<<<<< HEAD
-            <h1 class='heading'>COLOUR ANXIETY</h1>
-=======
+             <div>
+            <h1>COLOUR ANXIETY</h1>
+
             <h2>COLOUR ANXIETY</h2>
-          
->>>>>>> bea2ff1c665d36e0de282c93568f306ceacd5c93
-          );
+          </div>
+
+        ) 
     }
 }
  
