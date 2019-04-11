c30722732cb88af5b2ac41a9c5b6dbf24e2951c8
SharleneRyan <sa.ryan@live.com>
2019-04-12 11:23:12 +1200

    adding win and Lose

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ class Header extends Component {
     }
     render() { 
         return (
-            <h1>COLOUR ANXIETY</h1>
+            <h1 class='heading'>COLOUR ANXIETY</h1>
           );
     }
 }
