d33f50ad1434d1dd21e824bc9f745078bb64c9d1
ninja-nanz <nanz89@gmail.com>
2019-04-12 15:19:42 +1200

    Updates

---
+++ data.js
@@ -0,0 +1,53 @@
+const colour = [{
+  id:1,
+  word:"RED",
+  color: 'blue'
+},
+{
+  id:2,
+  word:"YELLOW",
+  color: 'green'
+},
+{
+  id:3,
+  word:"GREEN",
+  color: 'red'
+},
+{
+  id:4,
+  word:"BLUE",
+  color: 'yellow'
+},
+{
+  id:5,
+  word:"RED",
+  color: 'green'
+},
+{
+  id:6,
+  word:"BLUE",
+  color: 'red'
+},
+{
+  id:7,
+  word:"YELLOW",
+  color: 'blue'
+},
+{
+  id:8,
+  word:"GREEN",
+  color: 'red'
+},
+{
+  id:9,
+  word:"BLUE",
+  color: 'yellow'
+},
+{
+  id:10,
+  word:"RED",
+  color: 'blue'
+}
+]
+
+export default colour
